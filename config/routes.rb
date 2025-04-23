50704c440beb157083f9dec8af2bf9e709f160c0
Katie Bridson <kbridson@memphis.edu>
2025-04-23 12:25:44 -0500

    Updated app with static assets (image + css) and root route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root to: redirect("/users")
+
   get 'users', to: 'users#index', as: 'users'
   get 'users/:id', to: 'users#show', as: 'user'
 end
